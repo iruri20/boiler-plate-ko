7c2cc518be393071b979dcbcab82fad983de5368
iruri <hsyoon0@gmail.com>
2021-08-29 13:22:51 +0900

    처음 저장소에 올림

---
+++ models/User.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        maxlength: 50
+    },
+    email: {
+        type: String,
+        trim: true,     // 공간을 없애줌  (문자열 중간 포함)
+        unique: 1
+    },
+    password: {
+        type: String,
+        maxlength: 50
+    },
+    role: {
+        type: Number,
+        default: 0
+    },
+    image: String,
+    token: String,
+    tokenExp: Number,
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = { User }
